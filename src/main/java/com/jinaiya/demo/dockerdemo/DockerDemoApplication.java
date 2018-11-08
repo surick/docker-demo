a854873daf1fc0a33030ad5f6deb1081df924c29
surick <jk103@qq.com>
2018-11-08 17:29:28 +0800

    init

---
+++ src/main/java/com/jinaiya/demo/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.jinaiya.demo.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@RequestMapping("")
+	public String hello() {
+		return "hello docker！";
+	}
+}
